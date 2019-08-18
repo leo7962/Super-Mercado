b8702a1f1bcca584051572eb5146bc13ce0b8860
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-08-18 00:30:30 -0500

    Se realiza la primera étapa para la implementación a la base de datos, falta conexión y enrutamiento hacia a ella.

--- Super Mercado/Models/Detalle_Pedido.cs
+++ Super Mercado/Models/Detalle_Pedido.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Super_Mercado.Models
 {
     public class Detalle_Pedido
-{
+    {
         public long Id { get; set; }
 
         [Required]
@@ -15,5 +11,5 @@ namespace Super_Mercado.Models
 
         [Required]
         public long Id_Producto { get; set; }
-}
+    }
 }
