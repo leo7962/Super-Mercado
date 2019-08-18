e46505b186efab389f8d898ff692e69df0b64cbd
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-08-18 00:05:38 -0500

    Se establecen los modelos del proyecto.

---
+++ Super Mercado/Models/Detalle_Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Super_Mercado.Models
+{
+    public class Detalle_Pedido
+{
+        public long Id { get; set; }
+
+        [Required]
+        public long Id_Pedido { get; set; }
+
+        [Required]
+        public long Id_Producto { get; set; }
+}
+}
