8908901032e0494301efe90b971593f50c915126
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-08-18 01:29:18 -0500

    Se establece los primeros pasos para la conexión a base de datos, usando una DB Local.

---
+++ Super Mercado/Services/Super_MercadoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Super_Mercado.Models;
+
+namespace Super_Mercado.Services
+{
+    interface Super_MercadoService
+{
+}
+}
--- Super Mercado/Startup.cs
+++ Super Mercado/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Super_Mercado.Data;
 
 namespace Super_Mercado
 {
@@ -30,6 +27,8 @@ namespace Super_Mercado
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<Super_MercadoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Super_MercadoDevs")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
